7435de933b711fc484fe030e4d5d299f65f772f7
Celia Manzano <celiamc96@gmail.com>
2025-01-27 21:22:21 +0100

    Solved Lab

---
+++ functions.py
@@ -0,0 +1,54 @@
+def get_unique_list_f(lst):
+    """
+    Takes a list as an argument and returns a new list with unique elements from the first list.
+
+    Parameters:
+    lst (list): The input list.
+
+    Returns:
+    list: A new list with unique elements from the input list.
+    """
+    return list(set(lst))
+
+def count_case_f(string):
+    """
+    Returns the number of uppercase and lowercase letters in the given string.
+
+    Parameters:
+    string (str): The string to count uppercase and lowercase letters in.
+
+    Returns:
+    A tuple containing the count of uppercase and lowercase letters in the string.
+    """
+    uppercase_count = sum(1 for char in string if char.isupper())
+    lowercase_count = sum(1 for char in string if char.islower())
+    return (uppercase_count, lowercase_count)
+
+import string
+
+def remove_punctuation_f(sentence):
+    """
+    Removes all punctuation marks (commas, periods, exclamation marks, question marks) from a sentence.
+
+    Parameters:
+    sentence (str): A string representing a sentence.
+
+    Returns:
+    str: The sentence without any punctuation marks.
+    """
+    return ''.join(char for char in sentence if char not in string.punctuation)
+
+def word_count_f(sentence):
+    """
+    Counts the number of words in a given sentence. To do this properly, first it removes punctuation from the sentence.
+    Note: A word is defined as a sequence of characters separated by spaces. We can assume that there will be no leading or trailing spaces in the input sentence.
+    
+    Parameters:
+    sentence (str): A string representing a sentence.
+
+    Returns:
+    int: The number of words in the sentence.
+    """
+    cleaned_sentence = remove_punctuation_f(sentence)
+    words = cleaned_sentence.split()
+    return len(words)
